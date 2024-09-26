4d3147007e2c6ff440ad2397a6068061a092931f
jamesdstorr <jamesstorr@live.co.uk>
2024-09-26 16:10:45 +0100

    feat: Added BDD Test workflow

--- build/test-results/timeline/report.js
+++ build/test-results/timeline/report.js
@@ -1,4 +1,4 @@
 $(document).ready(function() {
-CucumberHTML.timelineItems.pushArray([{"id":"f4f44cd3-7736-41e8-906f-fa6a5dd74c76","feature":"Joke API Tests","scenario":"Get a Valid Joke Response","start":1727361646436,"group":1,"content":"","tags":"","end":1727361649908,"className":"passed"},{"id":"58bddcb7-da89-4111-9081-0d0700d8c08c","feature":"Joke API Tests","scenario":"Get a Chuck Norris Joke","start":1727361649912,"group":1,"content":"","tags":"","end":1727361649950,"className":"passed"}]);
+CucumberHTML.timelineItems.pushArray([{"id":"478eb4f2-eefd-41ff-a861-fb95e37d74d2","feature":"Joke API Tests","scenario":"Get a Valid Joke Response","start":1727363400608,"group":1,"content":"","tags":"","end":1727363403959,"className":"passed"},{"id":"8f5d6542-9e98-4b36-a809-9dcfa1a7658c","feature":"Joke API Tests","scenario":"Get a Chuck Norris Joke","start":1727363403964,"group":1,"content":"","tags":"","end":1727363403991,"className":"passed"}]);
 CucumberHTML.timelineGroups.pushArray([{"id":1,"content":"Thread[main,5,main]"}]);
 });
